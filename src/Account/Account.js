9c6cd0f43e8b9e8a00ea157ea444e378ae92323d
LorenzoLingardo <lingardolorenzo@gmail.com>
2021-01-28 19:04:57 -0700

    Fixed alignments of Account and sidebar/header

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -10,11 +10,15 @@ export default function Account() {
 
 function AccountButton(props) {
   return (
-    <b align="right">
-      <select>
-        <option id="Account"> {props.name}</option>,
-        <option> {props.staff} </option>,<option> {props.logout} </option>
-      </select>
-    </b>
+    <div class="dropdown">
+      <button onclick="HideDropDown()" class="dropbtn">
+        {" "}
+        Account{" "}
+      </button>
+      <div id="dropdown-content" class="dropdown-content">
+        <a href="">Staff Profile</a>
+        <a href="">Log Out</a>
+      </div>
+    </div>
   );
 }
