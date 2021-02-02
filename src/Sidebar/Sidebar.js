ca012c585513a8acaa39e257c67b7698a349648f
LorenzoLingardo <lingardolorenzo@gmail.com>
2021-02-02 14:29:01 -0700

    Fixed side-bar alignment, font, and highlight color

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -2,24 +2,26 @@ import React from "react";
 
 export default function Sidebar() {
   return (
-    <div class="side-bar">
+    <div class="side-bar-top">
       <SideBarHeader name="Dashboard" />
       <SideBarHeader name="Risk Domains" />
-      <SideBarTabs option="Accreditation Participation Requirements" /> <br />
-      <SideBarTabs option="Care, Treatment, & Services" /> <br />
-      <SideBarTabs option="Emergency Management" /> <br />
-      <SideBarTabs option="Environment of Care" /> <br />
-      <SideBarTabs option="Human Resources Management" /> <br />
-      <SideBarTabs option="Infection Prevent and Control" /> <br />
-      <SideBarTabs option="Information Management" /> <br />
-      <SideBarTabs option="Leadership" /> <br />
-      <SideBarTabs option="Life Safety" /> <br />
-      <SideBarTabs option="Medication Management" /> <br />
-      <SideBarTabs option="National Patient Safety Goals" /> <br />
-      <SideBarTabs option="Performance Improvement" /> <br />
-      <SideBarTabs option="Record of Care, Treatment, & Services" /> <br />
-      <SideBarTabs option="Rights and Responsibilities of the Individual" /> <br />
-      <SideBarTabs option="Waived Testing" /> <br />
+      <div class ="side-bar-content">
+        <SideBarTabs option="Accreditation Participation Requirements" /> <br />
+        <SideBarTabs option="Care, Treatment, & Services" /> <br />
+        <SideBarTabs option="Emergency Management" /> <br />
+        <SideBarTabs option="Environment of Care" /> <br />
+        <SideBarTabs option="Human Resources Management" /> <br />
+        <SideBarTabs option="Infection Prevent and Control" /> <br />
+        <SideBarTabs option="Information Management" /> <br />
+        <SideBarTabs option="Leadership" /> <br />
+        <SideBarTabs option="Life Safety" /> <br />
+        <SideBarTabs option="Medication Management" /> <br />
+        <SideBarTabs option="National Patient Safety Goals" /> <br />
+        <SideBarTabs option="Performance Improvement" /> <br />
+        <SideBarTabs option="Record of Care, Treatment, & Services" /> <br />
+        <SideBarTabs option="Rights and Responsibilities of the Individual" /> <br />
+        <SideBarTabs option="Waived Testing" /> <br />
+      </div>
     </div>
   );
 }
